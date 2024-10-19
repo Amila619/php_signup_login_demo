a45386d1050d95d98c4612979fddbf4f53ee5cf5
Amila619 <amilasandeepa349@gmail.com>
2024-10-19 13:16:19 +0530

    - greeting the user extracting the username from session_id - session_regenerate_id

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 
 session_start();
 
-// print_r($_SESSION);  
+if(isset($_SESSION["user_id"])){
+
+    $mysqli = require __DIR__ . "/database.php";
+
+    $sql = "SELECT * FROM user WHERE  id = {$_SESSION["user_id"]}";
+
+    $result = $mysqli->query($sql);
+
+    $user = $result->fetch_assoc();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,8 +29,8 @@ session_start();
     <div class="text-center grid gap-4">
         <h1 class="text-4xl uppercase">Home</h1>
 
-        <?php if (isset($_SESSION["user_id"])): ?>
-            <p>You are logged in.</p>
+        <?php if (isset($user)): ?>
+            <p>Hello <?= htmlspecialchars($user["name"]) ?> are logged in.</p>
             <p><a href="logout.php">Log out</a></p>
         <?php else: ?>
             <p><a href="login.php">Log In</a> or <a href="signup.html">Sign Up</a></p>
