91e44185fc6fd5c9ee8b06e956493ee9cba2e655
Amila619 <amilasandeepa349@gmail.com>
2024-10-19 13:06:25 +0530

    - added logout.php

--- index.php
+++ index.php
@@ -2,26 +2,31 @@
 
 session_start();
 
-print_r($_SESSION);  
-
+// print_r($_SESSION);  
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/output.css">
     <title>Home</title>
 </head>
+
 <body class="h-screen flex justify-center items-center">
-    <h1 class="text-4xl text-center uppercase">Home</h1>
+    <div class="text-center grid gap-4">
+        <h1 class="text-4xl uppercase">Home</h1>
 
-    <?php if (isset($_SESSION["user_id"])): ?>
-        <p>You are logged in.</p>
-    <?php else: ?>
-        <p><a href="login.php">Log In</a> or <a href="signup.html">Sign Up</a></p>
-    <?php endif; ?>
+        <?php if (isset($_SESSION["user_id"])): ?>
+            <p>You are logged in.</p>
+            <p><a href="logout.php">Log out</a></p>
+        <?php else: ?>
+            <p><a href="login.php">Log In</a> or <a href="signup.html">Sign Up</a></p>
+        <?php endif; ?>
 
+    </div>
 </body>
+
 </html>
